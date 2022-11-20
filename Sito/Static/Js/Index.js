758c7ee36d288cfed6de7bff2f875505d781bfdd
Daniel-ui-bit <114742565+Daniel-ui-bit@users.noreply.github.com>
2022-11-20 17:23:40 +0100

    Bottoni+ Css + Js completati

---
+++ Sito/Static/Js/Index.js
@@ -0,0 +1,19 @@
+function changebg(i){
+
+    switch(i){
+        case 1: document.body.style.backgroundColor = "#FE0202";
+            break;
+        case 2: document.body.style.backgroundColor = "#59BC98";
+            break;
+        case 3: document.body.style.backgroundColor = "#2594D2";
+            break;
+        case 4: document.body.style.backgroundColor = "#72BC59";
+            break;
+        case 5: document.body.style.backgroundColor = "#BC59A0";
+            break;
+        case 6: document.body.style.backgroundColor = "#DCF73C";
+            break;
+    }
+
+
+}
